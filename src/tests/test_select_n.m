a5404e279b1a75fd4cc403d69a141039d918ae44
mjp <mike.pekala@jhuapl.edu>
2016-05-17 15:22:47 -0400

    added tests for the three argument form

--- src/tests/test_select_n.m
+++ src/tests/test_select_n.m
@@ -17,7 +17,24 @@ assert(sum(yTest==3) == n);
 
 % make sure you get a reasonable result if you ask for too many
 % items.
-
 y = ones(10,1);
 bits = select_n(y, length(y)+1);
 assert(sum(bits) == length(y));
+
+
+% Try out the three argument form.
+n = 5;
+y = [ones(2*n,1) ; 2*ones(2*n,1) ; 3*ones(2*n,1)];
+
+[foo,bar] = select_n(y, n, n);
+for ii = 1:3
+    assert(sum(y(foo)==ii) == sum(y(bar)==ii));
+end
+
+
+[foo,bar] = select_n(y, n+1, n);
+for ii = 1:3
+    assert(sum(y(foo)==ii) == n+1);
+    assert(sum(y(bar)==ii) == n-1);
+end
+
