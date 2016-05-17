e4aa2e7c4dd3557f591394f760867641a8eee22d
mjp <mike.pekala@jhuapl.edu>
2016-05-17 15:09:30 -0400

    added another test case

--- src/tests/test_select_n.m
+++ src/tests/test_select_n.m
@@ -13,3 +13,11 @@ assert(sum(yTrain==3) == n);
 assert(sum(yTest==1) == n);
 assert(sum(yTest==2) == n);
 assert(sum(yTest==3) == n);
+
+
+% make sure you get a reasonable result if you ask for too many
+% items.
+
+y = ones(10,1);
+bits = select_n(y, length(y)+1);
+assert(sum(bits) == length(y));
