1e93ad8fa5742b4724ee03e54c7c4b74910817a1
mjpekala <mike.pekala@jhuapl.edu>
2016-09-27 13:16:09 -0400

    added visualization of a few Gabor features

--- src/examples/demo_gabor.m
+++ src/examples/demo_gabor.m
@@ -2,6 +2,7 @@
 %f = double(imread('cloth.tiff'))/255;
 f = double(imread('Lena.jpg'))/255;
 plot_help(f)
+title('input image');
 
 % choose parameter
 M = size(f,1);
@@ -18,3 +19,10 @@ tic;
 coeff = Gabor_transform(f,G);
 toc;
 
+% mjp - added.
+assert(size(f,1) == size(coeff,1));
+assert(size(f,2) == size(coeff,2));
+for ii = [1 2 10 50 size(coeff,3)]
+    plot_help(coeff(:,:,ii));
+    title(sprintf('Gabor feature dimension %d', ii));
+end
