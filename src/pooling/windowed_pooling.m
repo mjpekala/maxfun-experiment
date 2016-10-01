fa12c17106f43dce67f1ffb26e81bc1aad4c21a8
mjpekala <mike.pekala@jhuapl.edu>
2016-10-01 14:32:41 -0400

    updated comments

--- src/pooling/windowed_pooling.m
+++ src/pooling/windowed_pooling.m
@@ -1,9 +1,19 @@
 function Xp = windowed_pooling(X, poolDim, f_pool_wi)
 %  WINDOWED_POOLING  Pools over disjoint spatial regions.
 %
+%   X : an (m x n) matrix, 
+%            - or -
+%       an (m x n x d) matrix with 2 spatial dimensions and 1
+%                      feature dimension
+%
+%   poolDim   : the spatial pooling dimension (see pooling_regions)
+%   f_pool_wi : a "whole image" pooling function to apply to each region
+
+% mjp, oct 2016
 
 win_pool_2d = @(I) cell2mat(cellfun(f_pool_wi, pooling_regions(I, poolDim), ...
                                     'UniformOutput', 0));
+
 
 if ndims(X) == 2
     Xp = win_pool_2d(X);
