d7a6f8bf3bde28cdd012077ebe9fbbadd3889d0a
mjpekala <mike.pekala@jhuapl.edu>
2016-10-02 10:59:19 -0400

    added the new toTensor argument

--- src/pooling/pooling_regions.m
+++ src/pooling/pooling_regions.m
@@ -1,25 +1,33 @@
-function R = pooling_regions(X, poolDim)
+function R = pooling_regions(X, poolDim, toTensor)
 % POOLING_REGIONS Partitions an image X into disjoint pooling
 %                 regions (aka regions of interest).
 %
 %    R = pooling_regions(X, poolDim)
 %
 %    where,
-%       X       := an (m x n) image with 2 spatial dimensions
-%                    - or -
-%                  an (m x n x d) image with 2 spatial dimensions
-%                                 and 1 feature dimension
+%       X        := an (m x n) image with 2 spatial dimensions
+%                     - or -
+%                   an (m x n x d) image with 2 spatial dimensions
+%                                  and 1 feature dimension
 %
-%       poolDim := the side length of each square pooling region
+%       poolDim  := the side length of each square pooling region
 %
-%       R       := an (a x b) cell array where R(a,b) corresponds
-%                  to the (a,b)th pooling region from X.
+%       toTensor := a boolean; if true, reshapes result into a tensor
+%
+%       R       := one of (depends on toTensor):
+%                   1. an (a x b) cell array where R(a,b) corresponds
+%                      to the (a,b)th pooling region from X.
+%                   2. a (poolDim x poolDim x a*b) tensor where
+%                      the pooling regions have been stacked
+%                      along the third dimension.
 %
 %    This function is essentially just a wrapper around mat2cell
 %    that pads the input image so that it can be partitioned into
 %    blocks of size poolDim^2.
 
 % mjp, april 2016
+
+if nargin < 3, toTensor = false; end
 
 
 % pad X so that its spatial dimension are multiples of poolDim
@@ -47,4 +55,8 @@ else
     R = mat2cell(X, poolDim * ones(nPoolH,1), poolDim * ones(nPoolW,1), size(X,3));
 end
 
-             
+% (optional) stack the pooling regions up
+if toTensor
+    R = cat(3, R{:});
+end
+
