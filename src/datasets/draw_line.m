a5cc0a4aa82c58877135ad4a262de6be1950d6c8
Michael Pekala <mike.pekala@jhuapl.edu>
2016-10-23 14:37:03 -0400

    now draws multiple lines

--- src/datasets/draw_line.m
+++ src/datasets/draw_line.m
@@ -1,25 +1,33 @@
-function X = draw_line(X, x1, y1, x2, y2, value)
+function X = draw_line(X, p0, p1, value)
 %  DRAW_LINE  Draws a line on matrix data.
 %
 %   A more serious implementation might use
 %   Bresenham's algorithm:
 %     https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
 %
-%   X     : an (m x n) matrix.
-%   x1,y1 : the starting point (in matrix coordinates)
-%   x2,y2 : the ending point 
+%   X     : (m x n) matrix to draw lines on (treated as quadrant 4)
+%   p0    : (p x 2) matrix of start points (x,y) 
+%   p1    : (p x 2) matrix of end points (x,y)
 %   value : the value to write in the matrix X
 
 [m,n] = size(X);
-assert(1 <= x1 && 1 <= x2 && 1 <= y1 && 1 <= y2);
-assert(x1 <= n && x2 <= n && y1 <= m && y2 <= m);
+assert(all(1 <= p0(:,1)) && all(p0(:,1) <= n));
+assert(all(1 <= p1(:,1)) && all(p1(:,1) <= n));
+assert(all(1 <= p0(:,2)) && all(p0(:,2) <= m));
+assert(all(1 <= p1(:,2)) && all(p1(:,2) <= m));
 
-slope = (y2-y1) / (x2 - x1);
+x0 = p0(:,1);  y0 = p0(:,2);
+x1 = p1(:,1);  y1 = p1(:,2);
 
-dx = (x2-x1)/1000;
+slope = (y1 - y0) ./ (x1 - x0);
 
-x = x1:dx:x2;
-y = y1+slope*(x-x1);
-ind = sub2ind(size(X), round(y), round(x));
+for ii = 1:length(x0)
+    dx = (x1(ii) - x0(ii))/1000;
+    x = x0(ii):dx:x1(ii);
+    y = y0(ii) + slope(ii)*(x-x0(ii));
+    ind = sub2ind(size(X), round(y), round(x));
+    X(ind) = value;
+end
 
-X(ind) = value;
+
+
