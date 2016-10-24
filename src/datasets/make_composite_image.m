e27512841b11591677d067b28aa10c814a71170e
Michael Pekala <mike.pekala@jhuapl.edu>
2016-10-24 12:23:55 -0400

    modifying slightly

--- src/datasets/make_composite_image.m
+++ src/datasets/make_composite_image.m
@@ -6,19 +6,34 @@ assert(all(dim > size(X_texture)));
 
 [r,c] = size(X_texture);
 
-X = zeros(dim,dim);
+if 1
+    X = zeros(dim,dim);
+else
+    % background = gaussian noise
+    X = 80*rand(dim,dim);
+end
+
+
+
+% transform texture (optional)
+if 1
+    if rand() < .5, X_texture = fliplr(X_texture); end
+    if rand() < .5, X_texture = flipud(X_texture); end
+end
+
+
+% add clutter edges
+if n_edges > 0
+    p0 = floor(1 + rand(n_edges,2) * dim);
+    p1 = floor(1 + rand(n_edges,2) * dim);
+
+    color = color_range(1) + rand()*(color_range(2) - color_range(1));
+
+    X = draw_lines(X, p0, p1, color);
+end
+
 
 % translate the texture into the overall image
 tr = 1 + floor((dim - r) * rand);
 tc = 1 + floor((dim - c) * rand);
 X(tr:tr+r-1, tc:tc+c-1) = X_texture;
-
-% draw some clutter
-p0 = floor(1 + rand(n_edges,2) * dim);
-p1 = floor(1 + rand(n_edges,2) * dim);
-
-color = color_range(1) + rand()*(color_range(2) - color_range(1));
-
-X = draw_lines(X, p0, p1, color);
-
-
