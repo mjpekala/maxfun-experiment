7ae93f4ddd36b44615144b9a7bdea1082ccdd8c7
Michael Pekala <mike.pekala@jhuapl.edu>
2016-10-24 15:37:57 -0400

    updated

--- src/examples/demo_gabor_vs_sift_tips.m
+++ src/examples/demo_gabor_vs_sift_tips.m
@@ -1,10 +1,17 @@
 
 
 %% load data
-data_dir = '../datasets/KTH_TIPS';
-sz = [200 200];
 
-data = load_image_dataset(data_dir, sz);
+if 0
+    data_dir = '../datasets/KTH_TIPS';
+    sz = [200 200];
+    data = load_image_dataset(data_dir, sz);
+else
+    data_dir = '../datasets/UMD_Composite';
+    sz = [100 100];
+    data = load_image_dataset(data_dir, sz);
+end
+
 
 
 %% setup feature extractors
@@ -24,8 +31,8 @@ G = Gabor_construct(p_.gabor.M, p_.gabor.b, p_.gabor.sigma);
 % limit # of gabor features to be the same as SIFT
 G = G(:,:,1:prod(p_.sift.geom));
 
-%gabor_xform = @(I) abs(Gabor_transform(I, G));
-gabor_xform = @(I) Gabor_transform(I, G);
+gabor_xform = @(I) abs(Gabor_transform(I, G));
+%gabor_xform = @(I) Gabor_transform(I, G);
 
 
 %% Some manual analysis
@@ -50,21 +57,23 @@ end
 
 
 %% 
-X_foil_gabor = map_image(data.X(:,:,data.y==1), gabor_xform);
-e_gabor_real = squeeze(sum(sum(sum(real(X_foil_gabor), 1), 2), 4));
-e_gabor_imag = squeeze(sum(sum(sum(imag(X_foil_gabor), 1), 2), 4));
-e_gabor_mag = squeeze(sum(sum(sum(abs(X_foil_gabor), 1), 2), 4));
+if 0
+    X_foil_gabor = map_image(data.X(:,:,data.y==1), gabor_xform);
+    e_gabor_real = squeeze(sum(sum(sum(real(X_foil_gabor), 1), 2), 4));
+    e_gabor_imag = squeeze(sum(sum(sum(imag(X_foil_gabor), 1), 2), 4));
+    e_gabor_mag = squeeze(sum(sum(sum(abs(X_foil_gabor), 1), 2), 4));
 
-figure; 
-stem(e_gabor_mag); 
-hold on; 
-plot(1:length(e_gabor_real), e_gabor_real, 'x');
-plot(1:length(e_gabor_imag), e_gabor_imag, 'd');
-hold off;
-legend('modulus', 'real', 'imag');
-xlabel('feature dimension'); ylabel('sum');
-title('gabor, y=foil');
+    figure; 
+    stem(e_gabor_mag); 
+    hold on; 
+    plot(1:length(e_gabor_real), e_gabor_real, 'x');
+    plot(1:length(e_gabor_imag), e_gabor_imag, 'd');
+    hold off;
+    legend('modulus', 'real', 'imag');
+    xlabel('feature dimension'); ylabel('sum');
+    title('gabor, y=foil');
 
-figure; stem(e_gabor_imag);
-xlabel('feature dimension'); ylabel('sum');
-title('imag(gabor) y=foil');
+    figure; stem(e_gabor_imag);
+    xlabel('feature dimension'); ylabel('sum');
+    title('imag(gabor) y=foil');
+end
