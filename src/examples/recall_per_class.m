d8252874987521d7d883bace7b268b0b4d66951f
mjpekala <mike.pekala@jhuapl.edu>
2016-11-01 15:19:23 -0400

    ** re-implemented

--- src/examples/recall_per_class.m
+++ src/examples/recall_per_class.m
@@ -1,50 +1,42 @@
-function recall_per_class(Y_hat_all, y, class_names)
-% RECALL_PER_CLASS  Displays per-class recall
+function [recall, y_all] = recall_per_class(y_hat_all, y)
+% RECALL_PER_CLASS  Reports per-class recall
 %
-%    Y_hat_all   : an (m x n) matrix where m is the number of
-%                  examples in the data set an n is the number of 
-%                  algorithms that one used to evaluate each of 
-%                  the m instances.
+%    y_hat_all   : an (m x n x t) matrix where:
+%                       m is the number of examples in the data set, 
+%                       n is the number of algorithms used, and
+%                       t is the number of trials
 %
-%    y           : an (m x 1) vector of true class labels.
-%
-%    class_names : a (p x 1) cell array of class names, where
-%                  p is the total number of classes in the data set.
+%    y           : an (m x t) matrix of true class labels.
 %                 
 
-assert(length(y) == size(Y_hat_all,1));
+[m,n,t] = size(y_hat_all);
+assert(size(y,1) == m);
+assert(size(y,2) == t);
 
-y_all = sort(unique(y));
 
-% create class names if needed
-if nargin >= 3
-    assert(length(class_names) == length(y_all));
-else
-    class_names = {};
+y_all = sort(unique(y(:)));
+
+recall = zeros(numel(y_all), n, t);
+
+% calculate per class metrics
+for tt = 1:t
     for ii = 1:length(y_all)
-        class_names{ii} = sprintf('y=%2d', y_all(ii)); 
+        idx = find(y(:,t) == y_all(ii));
+ 
+        for jj = 1:n
+            n_correct = sum(y_hat_all(idx,jj,tt) == y(idx));
+            recall(ii,jj,tt) = 100 * n_correct / length(idx);
+        end
     end
 end
 
 
-% If Y_hat_all has more than 2 dimensions, reshape into an (m x n)
-% matrix.  An assumption is that all dimensions beyond the first 
-% correspond to independent experiments.
-if ndims(Y_hat_all > 2)
-    sz = size(Y_hat_all);
-    Y_hat_all = reshape(Y_hat_all, sz(1), prod(sz(2:end)));
-end
-
-
-% per class metrics
+% (optional) show results to stdout
 for ii = 1:length(y_all)
-    fprintf('  %14s  |', class_names{ii});
-    idx = find(y == y_all(ii));
- 
-    for jj = 1:size(Y_hat_all,2)
-        n_correct = sum(Y_hat_all(idx,jj) == y(idx));
-        recall = 100 * n_correct / length(idx);
-        fprintf(' %6.2f (%3d) |', recall, n_correct);
+    fprintf(' y = %3d |', y_all(ii));
+    for jj = 1:n
+        fprintf(' %6.2f (%6.2f)|', mean(recall(ii,jj,:)), std(recall(ii,jj,:)));
     end
     fprintf('\n');
 end
+
