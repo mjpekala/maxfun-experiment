77e9f509b1bbc3066b3dbb7d7eb4f51e12d1b2b6
mjpekala <mike.pekala@jhuapl.edu>
2017-10-24 09:24:50 -0400

    now only runs one classifier.

--- rev2/classify_images.m
+++ rev2/classify_images.m
@@ -26,34 +26,53 @@
 
 rng(1066);
 
+%% Experiment parameters
 pc.feature_type = 'dyadic-edge';
 pc.alpha_all = linspace(0, 1, 11);
+pc.classifier = 'knn';
+
+
+%% setup & load data
+
+%  Some quick experimentation on dyadic-edge(20,20,20) seems to suggest that KNN 
+%  works as well as the SVM (with default parameters, probably linear?).  
+%
+%  In addition to running more quickly, KNN has the virtue of being a little more
+%  intuitive; since we are not trying to build a complete classifier but say something
+%  about pooling KNN may be better for our purposes.
+%
+%  However, the relative max/avg performance may differ between algos...
+%
+switch(lower(pc.classifier))
+  case 'svm'
+    build_classifier = @(X,y) fitcecoc(X, y, 'Standardize', 1, 'Kfold', 5);
+  case 'knn'
+    build_classifier = @(X,y) fitcknn(X, y, 'NumNeighbors', 5, 'Standardize', 1, 'KFold', 5);
+  otherwise
+    error('unknown classifier');
+end
 
 
 load(sprintf('feats_%s.mat', pc.feature_type));
 p % show parameters used to create data
 
+%% Evaluate for different values of alpha (interpolation between max and avg)
 X_max = feats.maxpool;
 X_avg = feats.avgpool;
 y = feats.y(:);
 
-% Evaluate for different alphas
 for ii = 1:length(pc.alpha_all)
-    alpha_i = pc.alpha_all(ii);
-
     % features used are somewhere between max and avg
+    alpha_i = pc.alpha_all(ii);
     Xi = alpha_i * X_max + (1 - alpha_i) * X_avg;
 
-    
-    %% SVM classifier
-    %  XXX: may want to check to make sure SVM defaults are reasonable...
     tic
-    model = fitcecoc(Xi', y, 'Kfold', 5);
-    fprintf('[%s]: took %0.2f seconds to fit SVMs for %s+%0.2f\n', mfilename, toc, pc.feature_type, alpha_i);
+    model = build_classifier(Xi', y);
+    fprintf('\n[%s]: took %0.2f seconds to fit SVMs for %s+%0.2f\n', mfilename, toc, pc.feature_type, alpha_i);
 
     y_hat = kfoldPredict(model);
-    acc_svm = sum(y(:) == y_hat(:)) / length(y_hat);
-    fprintf('[%s]: SVM classification accuracy with %s+%0.2f is %0.2f\n', mfilename, pc.feature_type, alpha_i, acc_svm);
+    acc = sum(y(:) == y_hat(:)) / length(y_hat);
+    fprintf('[%s]: classification accuracy with %s+%0.2f is %0.3f\n', mfilename, pc.feature_type, alpha_i, acc_svm);
 
     % store results for later analysis
     if ii == 1
@@ -61,19 +80,11 @@ for ii = 1:length(pc.alpha_all)
         acc_all = zeros(length(pc.alpha_all),1);
     end
     Y_hat_all(:,ii) = y_hat;
-    acc_all(ii) = acc_svm;
-   
-    
-    %% KNN classifier
-    tic
-    model = fitcknn(Xi', y, 'NumNeighbors', 5, 'Standardize', 1, 'KFold', 5);
-    fprintf('[%s]: took %0.2f seconds to fit KNN for %s+%0.2f\n', mfilename, toc, pc.feature_type, alpha_i);
-    y_hat = kfoldPredict(model);
-    acc_knn = sum(y(:) == y_hat(:)) / length(y_hat);
-    fprintf('[%s]: KNN classification accuracy with %s+%0.2f is %0.2f\n', mfilename, pc.feature_type, alpha_i, acc_svm);
+    acc_all(ii) = acc;
 end
 
-save('svm_results.mat', 'Y_hat_all', 'feats', '-v7.3');
+save('classification_results.mat', 'Y_hat_all', 'acc_all', 'feats', 'pc', '-v7.3');
+
 
 
 %% Some analysis of classification accuracy
