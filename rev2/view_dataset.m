4dbaa09e10d6ca35b0ed1e6421ae05f7282a5335
mjpekala <mike.pekala@jhuapl.edu>
2017-11-17 11:55:55 -0500

    updated to support non-RBG data.

--- rev2/view_dataset.m
+++ rev2/view_dataset.m
@@ -7,15 +7,22 @@ if nargin < 3, figdir = ''; end
 %% plot some images
 y_all = unique(y);
 
-for yi = y_all
+for yi = y_all(:)'
     idx = find(y == yi);
    
     idx = randsample(idx, 1);
-    
-    figure;
-    imagesc(X(:,:,:,idx));
-    title(sprintf('object %d, y=%d', idx, yi));
-    set(gca, 'XTick', [], 'YTick', []);
+   
+    if size(X,3) == 3
+        figure;
+        imagesc(X(:,:,:,idx));
+        title(sprintf('object %d, y=%d', idx, yi));
+        set(gca, 'XTick', [], 'YTick', []);
+    else
+        figure;
+        imagesc(X(:,:,1,idx));
+        title(sprintf('object %d, feature 1, y=%d', idx, yi));
+        set(gca, 'XTick', [], 'YTick', []);
+    end
   
     if length(figdir)
         fn = sprintf('class_%02d_example.png', yi);
