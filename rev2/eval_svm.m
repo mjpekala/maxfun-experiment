27111ab9b80f1a947c4ff6ee2328e85c59ba5778
mjpekala <mike.pekala@jhuapl.edu>
2017-11-28 10:45:39 -0500

    changed the API; now assumes explicit train/test split

--- rev2/eval_svm.m
+++ rev2/eval_svm.m
@@ -1,12 +1,14 @@
-function acc = eval_svm(X, y, desc)
+function acc = eval_svm(X, y, is_train, desc)
 % EVAL_SVM  Trains and tests an SVM on a data set.
 
 rng(1066);  % for consistent results across invocations
 
-model = fitcecoc(X, y, 'Kfold', 5);
-y_hat = kfoldPredict(model);
+model = fitcecoc(X(:,is_train)', y(is_train));
+y_hat = predict(model, X(:, ~is_train)');
 
-acc = sum(y(:) == y_hat(:)) / numel(y);
+y_test = y(~is_train);
+
+acc = sum(y_test(:) == y_hat(:)) / numel(y_test);
 
 if length(desc)
     fprintf('[%s]: "%s" accuracy: %0.4f\n', mfilename, desc, acc);
