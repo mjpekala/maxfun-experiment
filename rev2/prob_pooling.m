e694603443403be3e38c2a2ee94703e5e79bb7b2
mjpekala <mike.pekala@jhuapl.edu>
2017-11-28 09:24:09 -0500

    added

---
+++ rev2/prob_pooling.m
@@ -0,0 +1,23 @@
+function out = prob_pooling(X)
+% PROB_POOLING  The weighted-by-probability pooling of [zei13]
+%
+%    X      : A single image (rows x cols x n_channels)
+%
+%  See equations (4),(6) in [zei13].
+%
+%  REFERENCES
+%    [zei13] Zeiler and Fergus "Stochastic Pooling for Regularization
+%    of Deep Convolutional Neural Networks" 2013.
+
+
+% mjp, november 2017
+
+[r,c,n] = size(X);
+
+out = zeros(n,1);
+
+for ii = 1:n
+    a_i = X(:,:,ii);                % spatial pooling region
+    p_i = a_i / sum(a_i(:));        % normalized activations; equation (4) in [zei13]
+    out(ii) = sum(sum(p_i .* a_i)); % equation (6) in [zei13]
+end
