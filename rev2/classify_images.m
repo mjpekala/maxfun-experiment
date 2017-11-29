bce2d5f1ccd037e42724acc012ecd866d208d8ea
mjpekala <mike.pekala@jhuapl.edu>
2017-11-28 22:07:30 -0500

    changed how hyperparameter selection is done.

--- rev2/classify_images.m
+++ rev2/classify_images.m
@@ -29,8 +29,6 @@ rng(1066);
 
 %% Experiment parameters
 pc.feature_type = 'raw';
-pc.alpha_all = linspace(0, 1, 11);
-pc.classifier = 'svm';   % acc(SVM) > acc(KNN) for dyadic-edge(20,20,20)
 
 
 %% load data
@@ -45,43 +43,58 @@ fprintf('[%s]: Using %d train and %d test examples\n', ...
 
 
 
-%% Hyperparameter selection for "one window" MAXFUN approximation
+%% Hyperparameter selection 
+
+% "one window" MAXFUN approximation
 acc_best = -Inf; maxfun_oo_best_idx = 0;
 
 for ii = 1:size(feats.maxfun_oo,3)
     X_train = feats.maxfun_oo(:, cvo.training, ii);
     y_train = feats.y(cvo.training);
+    
     model = fitcecoc(X_train', y_train, 'KFold', 3);
     y_hat = kfoldPredict(model);
     acc = sum(y_hat(:) == y_train(:)) / numel(y_train);
+    
     if acc > acc_best
-        fprintf('[%s]: maxfun_OO accuracy improved to %0.3f with %d\n', mfilename, acc, ii);
+        fprintf('[%s]: maxfun_OO accuracy improved to %0.3f with index %d\n', mfilename, acc, ii);
         acc_best = acc;
         maxfun_oo_best_idx = ii;
     end
 end
 
+% mixed pooling needs to select alpha
+alpha_all = linspace(0, 1, 11);
+acc_best = -Inf; alpha_best = 0;
+
+for alpha = alpha_all
+    X_train = feats.avgpool(:, cvo.training)*(1-alpha) + ...
+              feats.maxpool(:, cvo.training)*alpha;
+    y_train = feats.y(cvo.training);
+    
+    model = fitcecoc(X_train', y_train, 'KFold', 3);
+    y_hat = kfoldPredict(model);
+    acc = sum(y_hat(:) == y_train(:)) / numel(y_train);
+    
+    if acc > acc_best
+        fprintf('[%s]: mixed pooling accuracy improved to %0.3f with alpha=%0.1f\n', mfilename, acc, alpha);
+        acc_best = acc;
+        alpha_best = alpha;
+    end
+end
+
+X_mixed = feats.avgpool*(1-alpha_best) + feats.maxpool*alpha_best;
 
 
 %% Evaluate performance of baseline strategies
 
-% mixed pooling requires some hyperparameter selection
-tic
-alpha_pool = select_pool_alpha(feats.avgpool(:, cvo.training),...
-                               feats.maxpool(:, cvo.training), ...
-                               feats.y(cvo.training));
-toc
-
-X_mixed = feats.avgpool*(1-alpha_pool) + feats.maxpool*alpha_pool;
-
 eval_svm(feats.avgpool, feats.y, cvo.training, 'average pooling');
 eval_svm(feats.maxpool, feats.y, cvo.training, 'maximum pooling');
-eval_svm(X_mixed, feats.y, cvo.training, sprintf('mixed pooling strategy (%0.2f)', alpha_pool));
+eval_svm(X_mixed, feats.y, cvo.training, sprintf('mixed pooling strategy (%0.2f)', alpha_best));
 eval_svm(feats.probpool, feats.y, cvo.training, 'stochastic pooling');
 
 
-
-%% MAXFUN
+%% Evaluate MAXFUN (and variants)
 
 eval_svm(feats.maxfun, feats.y, cvo.training, 'MAXFUN pooling');
 eval_svm(feats.maxfun_oo(:,:,maxfun_oo_best_idx), feats.y, cvo.training, 'MAXFUN one window size');
