709f089db7ec734bbc3d02f9ff2ab8ad22f3a99b
Mike Pekala <mpekala@umd.edu>
2017-11-30 16:31:05 -0500

    updated comments

--- src/avg_pooling.m
+++ src/avg_pooling.m
@@ -1,6 +1,11 @@
 function out = avg_pooling(X)
-% AVG_POOLING  
+% AVG_POOLING   Implements spatial average pooling.
 %
-%    X      : A single image w/ dimensions (rows x cols x n_channels)
+%    X  : A single image w/ dimensions (rows x cols x n_channels)
+%   out : a n_channels dimensional vector of pooled values
+%
+
+% mjp, nov 2017
+
 [r,c,n] = size(X);
 out = mean(reshape(X, r*c, n));
