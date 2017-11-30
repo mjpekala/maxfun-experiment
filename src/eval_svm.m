79bc5bcfed502ae4529e452e31739bba6a3621bc
Mike Pekala <mpekala@umd.edu>
2017-11-30 16:35:23 -0500

    updated comments

--- src/eval_svm.m
+++ src/eval_svm.m
@@ -1,5 +1,20 @@
 function acc = eval_svm(X, y, is_train, desc)
 % EVAL_SVM  Trains and tests an SVM on a data set.
+%
+%   X        : a (d x n) matrix of n examples, each with d features.
+%   y        : an n-dimensional vector of class labels
+%   is_train : an n-dimensional binary vector indicating which 
+%              examples are for training.
+%   desc     : a string description (only used for reporting)
+%
+%  RETURNS
+%   acc      : classification accuracy
+%
+%  The main value of this function is that it provdes an easy
+%  way to set the random number seed each time.  This helps
+%  with consistency when comparing across feature types.
+
+% mjp, november 2017
 
 rng(1066);  % for consistent results across invocations
 
