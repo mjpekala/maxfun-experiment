f37a092fc8f63ba2cae3b29ac656d97a9306cbce
Mike Pekala <mpekala@umd.edu>
2017-11-30 16:31:41 -0500

    updated comments

--- src/max_pooling.m
+++ src/max_pooling.m
@@ -1,6 +1,10 @@
 function out = max_pooling(X)
 % MAX_POOLING
 %
-%    X      : A single image w/ dimensions (rows x cols x n_channels)
+%    X  : A single image w/ dimensions (rows x cols x n_channels)
+%   out : a n_channels dimensional vector of pooled values
+
+% mjp, november 2017
+
 [r,c,n] = size(X);
 out = max(reshape(X, r*c, n));
