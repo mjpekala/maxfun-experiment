1e6c60a5f072a9e5d3ac6339cccf96b06980f7c4
mjpekala <mike.pekala@jhuapl.edu>
2017-12-01 09:03:04 -0500

    added.  this is just a template and needs to be implemented

---
+++ src/centered_maxfun_pooling.m
@@ -0,0 +1,15 @@
+function out = centered_maxfun_pooling(X)
+% CENTERED_MAXFUN_POOLING  Pooling inspired by the discrete maximal function.
+%
+%    X   : A single image w/ dimensions (rows x cols x n_channels)
+%
+%  RETURNS:
+%    out : (1 x n_channels) vector of pooled values 
+
+
+[n_rows, n_cols, n_channels] = size(X);
+
+out = zeros(1,n);
+
+error('this needs to be implemented!');
+
